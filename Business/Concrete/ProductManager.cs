869c371b9a79f2d25f179cb14fce7ef3505dc31e
Kokimen <dev.burakkoktay@outlook.com>
2021-02-10 15:17:38 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService //manager gördüysen iş katmanının somut hali
+    {
+        IProductDal _productDal; //bir iş katmanı başka bir iş katmanını yenileyemediği için bunu yazıyoruz ve ctor generate ediyoruz
+
+        public ProductManager(IProductDal productDal) //injection
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş Kodları
+            //Yetkisi var mı?
+            return _productDal.GetAll();
+            
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products; //alt çizgi global liste anlamına gelir
+        public InMemoryProductDal()
+        {
+           //Oracle, Sql Server, MongoDb'den bir veri geliyormuş gibi simülasyon
+            _products = new List<Product> 
+            {
+                new Product{ProductID=1, CategoryId=1, ProductName="Bardak", UnitPrice=15,UnitsInStock=15},
+                new Product{ProductID=1, CategoryId=1, ProductName="Kamera", UnitPrice=500,UnitsInStock=3},
+                new Product{ProductID=1, CategoryId=2, ProductName="Telefon", UnitPrice=1500,UnitsInStock=2},
+                new Product{ProductID=1, CategoryId=2, ProductName="Klavye", UnitPrice=150,UnitsInStock=65},
+                new Product{ProductID=1, CategoryId=2, ProductName="Fare", UnitPrice=85,UnitsInStock=1}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+        //LINQ - Language Integrated Query , gömülmüş dil
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductID==product.ProductID); //=> bu işarete lambda denir, foreach görevi görür
+            //SingleOrDefault kodu tek tek foreach varmışçasına listeyi dolaşır
+            _products.Remove(productToDelete);
+        }
+
+        
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p=> p.CategoryId == categoryId).ToList(); //where, içindeki şarta uyan tüm elemanları yeni bir liste haline getirir ve döndürür
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderdiğim ürün id'sine sahip olan listedeki ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p=>p.ProductID==product.ProductID);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
