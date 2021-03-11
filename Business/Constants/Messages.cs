ccf7e648e47e231b7acf35776ffb92e848d86a4b
Kokimen <dev.burakkoktay@outlook.com>
2021-03-11 13:47:25 +0300

    14th

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,6 @@
-﻿namespace Business.Constants
+﻿using Core.Entities.Concrete;
+
+namespace Business.Constants
 {
     public static class Messages //static verince new olmaz, tek bir mesaj vereceğiz bitecek o yüzden statik
     {
@@ -10,5 +12,11 @@
         public static string ProductNameAlreadyExists = "Product Name Already Exists";
         public static string CategoryCountError = "Can't Add Products While Category Numbers Higher Than 15";
         public static string AuthorizationDenied = "Authorization Denied";
+        public static string UserRegistered = "User Registered";
+        public static string UserNotFound = "User Not Found";
+        public static string PasswordError = "Incorrect Password";
+        public static string SuccessfulLogin = "Successfull Login";
+        public static string UserAlreadyExists = "User Already Exist";
+        public static string AccessTokenCreated = "Access Token Created";
     }
 }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Core.Utilities.IoC;
+using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebAPI
 {
@@ -23,8 +28,24 @@ namespace WebAPI
             services.AddControllers();
             //services.AddSingleton<IProductService, ProductManager>(); //Eğer IProductService istenirse bana bir new ProductManager üret ve geri ver.
             //services.AddSingleton<IProductDal, EfProductDal>(); //Eğer IProductDal istenirse bana bir EfProductDal üret ve geri ver.
-        }
+            var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = tokenOptions.Issuer,
+                        ValidAudience = tokenOptions.Audience,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+                    };
+                });
+            ServiceTool.Create(services);
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -37,6 +58,8 @@ namespace WebAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
